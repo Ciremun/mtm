636ec1008cc0d8473853e64d0265c24f82339474
Ciremun <Ciremun@yandex.ru>
2022-09-01 14:04:45 +0300

    drag and drop

--- static/index.js
+++ static/index.js
@@ -1,5 +1,14 @@
 let selected_element = null;
 
+let construct_item = (src) => {
+    let image = new Image();
+    image.dataset.drag = '';
+    image.src = src;
+    image.draggable = false;
+    image.classList.add('item');
+    return image;
+};
+
 let insert_element = (selected, target) => {
     selected.remove();
     if ('items' in target.dataset)
@@ -41,20 +50,30 @@ window.addEventListener('mousedown', drag_start);
 window.addEventListener('mousemove', drag_move);
 window.addEventListener('mouseup', drag_end);
 
+window.addEventListener('drop', (e) => {
+    e.preventDefault();
+    let target_element = e.target;
+    if (['items', 'drag', 'row'].some((d) => d in target_element.dataset))
+    {
+        for (const file of e.dataTransfer.files)
+        {
+            let reader = new FileReader();
+            reader.onload = (e) => {
+                insert_element(construct_item(e.target.result), target_element);
+            }
+            reader.readAsDataURL(file);
+        }
+    }
+});
+window.addEventListener('dragover', (e) => { e.preventDefault(); });
+
 let to_rank_div = document.getElementById('to-rank');
 
 let input_images = (files) => {
     for (const file of files) 
     {
         let reader = new FileReader();
-        reader.onload = function (e) {
-            let image = new Image();
-            image.dataset.drag = '';
-            image.src = e.target.result;
-            image.draggable = false;
-            image.classList.add('item');
-            to_rank_div.append(image);
-        }
+        reader.onload = (e) => { to_rank_div.append(construct_item(e.target.result)); }
         reader.readAsDataURL(file);
     }
 }
