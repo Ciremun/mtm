8669056b11688f29b078d5a8d4586a2cdce8d90b
Ciremun <Ciremun@yandex.ru>
2022-09-04 20:24:09 +0300

    minify

--- build.py
+++ build.py
@@ -3,18 +3,6 @@
 if __name__ != '__main__':
     exit(1)
 
-# Minify CSS
-# from source.util import *
-
-# def minify(css_path: str, min_css_path: str) -> None:
-#     min_css = minify_css(css_path)
-#     if min_css:
-#         with open(min_css_path, 'w') as f:
-#             f.write(min_css)
-
-# minify('docs/static/main.css', 'docs/static/main.min.css')
-# minify('docs/static/fixed_footer.css', 'docs/static/fixed_footer.min.css')
-
 # Evaluate Template
 from jinja2 import Environment, FileSystemLoader
 
@@ -27,7 +15,8 @@ def build_page(name: str) -> None:
     with open(f'{name}.html', 'w', encoding='utf-8') as f:
         f.write(page)
 
-main_css = 'main.css'
+main_css = 'main.min.css'
+index_js = 'index.min.js'
 
 env = Environment(loader=FileSystemLoader('./templates'), trim_blocks=True, lstrip_blocks=True)
 
